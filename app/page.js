0fa20d77013048ad10af59d6108e3e7401060f7d
Florian <florian.pabler@icloud.com>
2024-08-11 18:05:59 +0200

    Added some Content to /

--- app/page.js
+++ app/page.js
@@ -1,9 +1,14 @@
 import styles from "./page.module.css";
+import React from "react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        <h1>HELLO WORLD!</h1>
+            <h2>HELLO WORLD!</h2>
+            <p>Hello Stranger, I'm Flo, the creator of this website.</p>
+            <p>Star me on GitHub or check out my bio i guess</p>
+            <Link href={"/bio"} className={styles.button}>Bio</Link>
     </main>
   );
 }
