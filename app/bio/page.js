b1320a08de3e0e041e0128acc18780006b767db2
Fl0.exe <florian.pabler@icloud.com>
2024-10-21 11:26:21 +0200

    Facelift layout

--- app/bio/page.js
+++ app/bio/page.js
@@ -1,24 +1,24 @@
 "use client";
 
-import { useEffect } from 'react';
+import {useEffect} from 'react';
 
 export default function Page() {
-  useEffect(() => {
-    const videoURL = "Never-Gonna.mp4";
+    useEffect(() => {
+        const videoURL = "/Never-Gonna.mp4";
 
-    function redirect() {
-      window.location.href = videoURL;
-    }
+        function redirect() {
+            window.location.href = videoURL;
+        }
 
-    redirect();
-  }, []);
+        redirect();
+    }, []);
 
-  return (
-    <main>
-      <video autoPlay loop width="100%">
-        <source src="Never-Gonna.mp4" type="video/mp4" />
-        Keine eingebetteten videos :(
-      </video>
-    </main>
-  );
+    return (
+        <main>
+            <video autoPlay loop width="100%">
+                <source src="/Never-Gonna.mp4" type="video/mp4"/>
+                Keine eingebetteten videos :(
+            </video>
+        </main>
+    );
 }
--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,24 @@
-import {Inter, JetBrains_Mono} from "next/font/google";
+import {JetBrains_Mono} from "next/font/google";
 import "./globals.css";
+import React from "react";
+import Navbar from "@/components/navbar/navbar";
 
-const inter = Inter({ subsets: ["latin"] });
-const jetBrains_mono = JetBrains_Mono({ subsets: ["latin"] });
+const jetBrains_mono = JetBrains_Mono({subsets: ["latin"]});
 
 export const metadata = {
-  title: "Pabler",
-  description: "This is crazy mega cool",
+    title: "Pabler",
+    description: "This is crazy mega cool",
 };
 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <body className={jetBrains_mono.className}>{children}</body>
-    </html>
-  );
+export default function RootLayout({children}) {
+    return (
+        <html lang="en">
+        <body className={jetBrains_mono.className}>
+        <Navbar/>
+        <div className={"childrenContainer"}>
+            {children}
+        </div>
+        </body>
+        </html>
+    );
 }
