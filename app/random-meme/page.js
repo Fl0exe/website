49b9b84a429315c4dfa0fc07aa95ec49ba6c3799
Fl0.exe <florian.pabler@icloud.com>
2024-10-22 11:34:34 +0200

    Add random-meme page

---
+++ app/random-meme/page.js
@@ -0,0 +1,21 @@
+import styles from "./page.module.css";
+
+async function fetchMeme() {
+    const response = await fetch(`https://meme-api.com/gimme?cache-bust=${Date.now()}`);
+    const data = await response.json();
+    return {
+        title: data.title,
+        url: data.url,
+    };
+}
+
+export default async function RandomMeme() {
+    const meme = await fetchMeme();
+
+    return (
+        <main className={styles.container}>
+            <h1>{meme.title}</h1>
+            <img className={styles.meme} src={meme.url} alt={meme.title} />
+        </main>
+    );
+}
