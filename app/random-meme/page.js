218cddcbd1b5c6be94d62f7bc1168a21084c08e5
Fl0.exe <florian.pabler@icloud.com>
2024-10-23 15:04:54 +0200

    Fix random meme not actually being random

--- app/random-meme/page.js
+++ app/random-meme/page.js
@@ -1,22 +1,14 @@
 import styles from "./page.module.css";
-import Image from "next/image";
-
-async function fetchMeme() {
-    const response = await fetch(`https://meme-api.com/gimme?cache-bust=${Date.now()}`);
-    const data = await response.json();
-    return {
-        title: data.title,
-        url: data.url,
-    };
-}
 
 export default async function RandomMeme() {
-    const meme = await fetchMeme();
+    const response = await fetch(`https://meme-api.com/gimme`, {cache: 'no-store'});
+    const data = await response.json();
+    const meme = {title: data.title, url: data.url};
 
     return (
         <main className={styles.container}>
             <h1>{meme.title}</h1>
-            <Image className={styles.meme} src={meme.url} alt={meme.title} width={2000} height={2000}/>
+            <img className={styles.meme} src={meme.url} alt={meme.title}/>
         </main>
     );
 }
