380e7e1196267a3c7d42382617be05ea914d0b6c
Florian Pabler <florian.pabler@icloud.com>
2025-07-21 19:50:01 +0200

    [fix] video url

--- app/bio/page.js
+++ app/bio/page.js
@@ -4,7 +4,7 @@ import {useEffect} from 'react';
 
 export default function Page() {
     useEffect(() => {
-        const videoURL = "/Never-Gonna.mp4";
+        const videoURL = "/never-gonna.mp4";
 
         function redirect() {
             window.location.href = videoURL;
@@ -16,8 +16,8 @@ export default function Page() {
     return (
         <main>
             <video autoPlay loop width="100%">
-                <source src="/Never-Gonna.mp4" type="video/mp4"/>
-                Keine eingebetteten videos :(
+                <source src="/never-gonna.mp4" type="video/mp4"/>
+                No embedded videos :(
             </video>
         </main>
     );
